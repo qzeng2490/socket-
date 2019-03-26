2e82ecf08edc770127e4250e74b8462a25659107
Qiang <qzeng2490@yahoo.com>
2019-03-26 12:58:09 +0800

    first commit

---
+++ ChatRoom/lib-clink/src/main/java/net/qiujuer/library/clink/core/Receiver.java
@@ -0,0 +1,17 @@
+package net.qiujuer.library.clink.core;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public interface Receiver extends Closeable {
+    void setReceiveListener(IoArgs.IoArgsEventProcessor processor);
+
+    boolean postReceiveAsync() throws IOException;
+
+    /**
+     * 获取读取数据的时间
+     *
+     * @return 毫秒
+     */
+    long getLastReadTime();
+}
---
+++ ChatRoom/lib-clink/src/main/java/net/qiujuer/library/clink/core/Sender.java
@@ -0,0 +1,17 @@
+package net.qiujuer.library.clink.core;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public interface Sender extends Closeable {
+    void setSendListener(IoArgs.IoArgsEventProcessor processor);
+
+    boolean postSendAsync() throws IOException;
+
+    /**
+     * 获取输出数据的时间
+     *
+     * @return 毫秒
+     */
+    long getLastWriteTime();
+}
